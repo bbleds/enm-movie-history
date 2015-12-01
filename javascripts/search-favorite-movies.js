f57211a631b5c92e428e40c22bc807e5bfa3d485
Stephanie Krause <schmitz1193@gmail.com>
2015-12-01 09:45:25 -0600

    range and 10 stars working

--- javascripts/search-favorite-movies.js
+++ javascripts/search-favorite-movies.js
@@ -3,6 +3,10 @@ define(function(require) {
 	var Q = require("q");
 	var _ = require("lodash");
 	var firebase = require("firebase");
+	var populateAllPage = require("get-users-movie-collection");
+	var getposter = require("get-movie-poster-data");
+
+
 
 // passing in the range # from the slider input to determine what movies to display - ssk
 
@@ -12,13 +16,25 @@ define(function(require) {
 			ref.on("value", function(snapshot) {
 				var collectionsRef = snapshot.val();
 				console.log("collectionsRef", collectionsRef);
-				var filteredWatched = _.filter(collectionsRef, function(obj) {
-					if (obj.Rating === range) {
-						return obj;
-					}
-				});
-				deferred.resolve(filteredWatched);
-				console.log("FilteredWatched", filteredWatched);
+				if (range === "0") {
+					populateAllPage(userid)
+			        .then(function(data) {
+			          var allUserMovies = Object.keys( data ).map(function(key) { return data[key];});
+			          var sortedResults = _.sortBy(allUserMovies, "Title");
+			          getposter.requestData(sortedResults);
+			        });
+
+				}
+				else {
+					var filteredWatched = _.filter(collectionsRef, function(obj) {
+						if (obj.Rating === range) {
+							return obj;
+						}
+					});
+					deferred.resolve(filteredWatched);
+					console.log("FilteredWatched", filteredWatched);
+					
+				}
 			});
 			return deferred.promise;
 		};//--end return function
